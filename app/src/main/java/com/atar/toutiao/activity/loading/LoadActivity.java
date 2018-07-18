845fe76ddf3f4aef0e515e73961c63f338075b31
helo <hello>
2018-07-18 14:09:24 +0800

    启动优化

--- app/src/main/java/com/atar/toutiao/activity/loading/LoadActivity.java
+++ app/src/main/java/com/atar/toutiao/activity/loading/LoadActivity.java
@@ -1,9 +1,11 @@
 package com.atar.toutiao.activity.loading;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.atar.toutiao.R;
+import com.atar.toutiao.activity.MainActivity;
 
 public class LoadActivity extends Activity {
 
@@ -11,5 +13,6 @@ public class LoadActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
